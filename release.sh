4ba68d1807eadbc90926e98bbf70d3184d6095a0
Jabran Rafique <jabran.rafique@ratedpeople.com>
2020-03-20 13:32:25 +0000

    add release script

---
+++ release.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+if [ -z "$1" ]
+  then
+  echo "- No version is given. Use as ./release.sh 1.0.0";
+  exit 1
+fi
+
+echo "- Clean up SVN";
+rm -rf fetchwitter
+
+echo "- Checkout from SVN remote";
+svn co http://plugins.svn.wordpress.org/fetchwitter/
+
+echo "- Clean up SVN trunk";
+rm -rf fetchwitter/trunk/*
+
+echo "- Copy latest code to SVN trunk";
+cp -r inc lib *.php *.md *.txt fetchwitter/trunk/
+svn add fetchwitter/trunk/*
+
+echo "- Create new SVN tag";
+svn cp fetchwitter/trunk "fetchwitter/tags/$1"
+
+echo "- Release to SVN remote";
+cd fetchwitter/
+svn ci -m "Release version $1" --username jabranr
